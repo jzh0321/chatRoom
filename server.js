5fc30361639ad79f9fe723392ca531cfbc6c0326
hu3shui <813116227@qq.com>
2017-01-13 21:46:28 +0800

    修改端口号为80

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ var express = require('express'),
 app.use('/', express.static(__dirname + '/www'));
 //bind the server to the 80 port
 //server.listen(3000);//for local test
-server.listen(process.env.PORT || 7788, () => {
+server.listen(process.env.PORT || 80, () => {
     "use strict";
-    console.log('开始监听7788...');
+    console.log('开始监听80...');
 });//publish to heroku
 //server.listen(process.env.OPENSHIFT_NODEJS_PORT || 3000);//publish to openshift
 //console.log('server started on port'+process.env.PORT || 3000);
